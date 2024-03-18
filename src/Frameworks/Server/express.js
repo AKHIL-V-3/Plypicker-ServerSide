fec87322e7a5fab1913f87df30dbaf6bf3888240
Akhil V <akhilv033@gmail.com>
2024-03-18 11:21:31 +0530

    updated the URLs

--- src/Frameworks/Server/express.js
+++ src/Frameworks/Server/express.js
@@ -9,7 +9,7 @@ require("dotenv").config()
 function expressConfig(app) {
 
       app.use(cors({
-            origin: 'http://localhost:5173',
+            origin: process.env.CLIENT_URL,
             credentials: true,
       }));
       app.use(cookieParser());
